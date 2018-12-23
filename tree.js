709690e0537cb64eed7cbaa99272dbb28496b95f
KJ <kj.schmidt913@gmail.com>
2018-12-23 17:59:02 -0500

    added code for tree diagram, but not functional yet

--- tree.js
+++ tree.js
@@ -25,8 +25,6 @@ var treeData = [
   }
 ];
 
-
-// ************** Generate the tree diagram	 *****************
 var margin = {top: 20, right: 120, bottom: 20, left: 120},
 	width = 960 - margin.right - margin.left,
 	height = 500 - margin.top - margin.bottom;
