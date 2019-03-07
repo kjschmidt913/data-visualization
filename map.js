05c7d80f7fd692dd600c011fcff3e6a553e4fb26
KJ <kj.schmidt913@gmail.com>
2019-03-07 09:02:40 -0400

    made projection var

--- map.js
+++ map.js
@@ -2,3 +2,10 @@ w = 3000;
 h = 1250;
 var minZoom;
 var maxZoom;
+
+var projection = d3
+   .geoEquirectangular()
+   .center([0, 15])
+   .scale([w/(2*Math.PI)]) // scale to fit group width
+   .translate([w/2,h/2]) // center in group
+;
