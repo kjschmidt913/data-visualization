dddeecdc4199ede420a799df107b1ff01f0e123c
KJ <kj.schmidt913@gmail.com>
2019-03-07 09:03:26 -0400

    added path

--- map.js
+++ map.js
@@ -4,8 +4,11 @@ var minZoom;
 var maxZoom;
 
 var projection = d3
-   .geoEquirectangular()
-   .center([0, 15])
-   .scale([w/(2*Math.PI)]) // scale to fit group width
-   .translate([w/2,h/2]) // center in group
-;
+    .geoEquirectangular()
+    .center([0, 15])
+    .scale([w / (2 * Math.PI)]) // scale to fit group width
+    .translate([w / 2, h / 2]); // center in group
+
+var path = d3
+    .geoPath()
+    .projection(projection);
