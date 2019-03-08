94a53f4e411c9a27d15ddd74a3db2e4b1801f32d
KJ <kj.schmidt913@gmail.com>
2019-03-08 09:24:36 -0400

    added zoom var, getTextBox function, and svg var to start drawing the map

--- map.js
+++ map.js
@@ -1,10 +1,11 @@
+// https://medium.com/@andybarefoot/making-a-map-using-d3-js-8aa3637304ee
+
 w = 3000;
 h = 1250;
 var minZoom;
 var maxZoom;
 
-var projection = d3
-    .geoEquirectangular()
+var projection = d3.geoEquirectangular()
     .center([0, 15])
     .scale([w / (2 * Math.PI)]) // scale to fit group width
     .translate([w / 2, h / 2]); // center in group
@@ -14,10 +15,26 @@ var path = d3
     .projection(projection);
 
 function zoomed() {
-    t = d3
-        .event
+    t = d3.event
         .transform;
     countriesGroup.attr(
         "transform", "translate(" + [t.x, t.y] + ")scale(" + t.k + ")"
     );
 }
+
+var zoom = d3.zoom()
+    .on("zoom", zoomed);
+
+function getTextBox(selection) {
+    selection.each(function (d) {
+        d.bbox = this.getBBox();
+    });
+}
+
+var svg = d3
+  .select("#map")
+  .append("svg")
+  .attr("width", $("#map").width())
+  .attr("height", $("#map").height())
+  .call(zoom)
+;
