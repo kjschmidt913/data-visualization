43772fddcbb4743476f74c2a808e6389a27c8948
KJ <kj.schmidt913@gmail.com>
2019-03-19 16:57:23 -0400

    added map projection

--- map.js
+++ map.js
@@ -3,3 +3,9 @@ h = 1250;
 // variables for catching min and max zoom factors
 var minZoom;
 var maxZoom;
+
+var projection = d3
+    .geoEquirectangular()
+    .center([0, 15])
+    .scale([w / (2 * Math.PI)])
+    .translate([w / 2, h / 2]);
